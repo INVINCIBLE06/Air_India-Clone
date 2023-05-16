1804807b75bfd68f2523bdbda706b5e006040b5f
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-16 18:58:24 +0530

    common functions will be written here in this folder

---
+++ helper/commonFetching.js
@@ -0,0 +1,28 @@
+const db = require('../models');
+const User = db.user;
+
+exports.userDetailsByEmail = async (email) => 
+{
+    try
+    {
+        const details = await User.findOne
+        ({
+            where:
+            {
+                email: email
+            }
+        });
+
+        if (!details) 
+        {
+            console.log(`#### No user found with this email ####`);
+        }
+        
+        return details;
+    }
+    catch (error)
+    {
+        console.log('Error retrieving user details:', error);
+        throw error;
+    }
+};
