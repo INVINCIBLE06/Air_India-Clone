fc2042854583a2547df6152148df4dd54ea5d1ae
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-16 14:39:26 +0530

    This time we will be storing the address of every user in different table

---
+++ models/address.model.js
@@ -0,0 +1,50 @@
+const constants = require('../utils/constants');
+
+module.exports = (sequelize, Sequelize) => 
+{
+    const Address = sequelize.define('addresses',
+    {
+        id :
+        {
+            type : Sequelize.INTEGER,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        plot_noORflat_no :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+        street_name :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+        city :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+        district :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+        state :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+        country :
+        {
+            type : Sequelize.STRING,
+            allowNull : false,
+        },
+    },
+    {
+        timestamps: false,
+    });
+
+    return Address
+
+};
