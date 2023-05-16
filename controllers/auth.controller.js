0596ddf309ff36c7f4c80f00c2fcb5effcf5a3f7
Saurabh Lalit Pande <sp832154@gmail.com>
2023-05-17 00:24:49 +0530

    Address data is now storing in the address table

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,52 +9,60 @@ const User = db.user;
 const Add = db.address
 
 
-exports.registration = async (req, res) =>
+exports.registration = async (req, res) => 
 {
-    let user_data =
+    try
     {
-        name : req.body.name,
-        username : req.body.username,
-        email : req.body.email,
-        contact_no : req.body.contact_no,
-        password : req.body.password,
-        date_of_birth : req.body.date_of_birth
+        let user_data = 
+        {
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+            contact_no: req.body.contact_no,
+            password: req.body.password,
+            date_of_birth: req.body.date_of_birth,
+        };
+        
+        const user = await User.create(user_data);
+        
+        if(user)
+        {
+            console.log(`#### New User '${user.name}' created ####`);
+            let id = await fetch.userDetailsByEmail(req.body.email);
+            let address_data = 
+            {
+                house_no: req.body.house_no,
+                street_name: req.body.street_name,
+                city: req.body.city,
+                district: req.body.district,
+                state: req.body.state,
+                country: req.body.country,
+            };
+            const address = await Add.create(address_data);
+            if(address)
+            {
+                console.log(`#### '${user.name}' address has been added in the database ####`);
+                await user.addAddress
+                ({
+                    userId : user.id,
+                    addressId : address.id
+                }); // Add the association between user and address
+                console.log(`#### Association between '${user.name}' and the address has been created ####`);
+            }
+            else
+            {
+                console.log(`#### Failed to add the address of new user '${user.name}' ####`);
+            }
+        }
+        else
+        {
+            console.log('#### Failed to create a new user ####');
+        }
     }
-    // console.log(user_data);
-    const user = await User.create(user_data);
-
-    if(user) 
+    catch(error)
     {
-        console.log(`#### New User '${user.name}' created ####`);
-    }
-    else
-    {
-        console.log('#### Failed to create a new user ####');
-    }
-
-    let id = await fetch.userDetailsByEmail(req.body.email);
-    
-    console.log("Here :- ", id.users.dataValues.id); 
-     
-    let address_data = 
-    {
-        plot_noORflat_no : req.body.plot_noORflat_no,
-        street_name : req.body.street_name,
-        user_Id	: id,
-        city : req.body.city,
-        district : req.body.district,
-        state : req.body.state,
-        country : req.body.country
-    }
-    // console.log(user_data);
-    const add = await Add.create(address_data);
-    
-    if(address_data)
-    {
-        console.log(`#### '${user.name}' address have been added in the database ####`);
-    }
-    else
-    {
-        console.log(`#### Failed to add the address of new user '${user.name}' ####`);
+      console.log('Error during registration:', error);
+      // Handle the error and send an appropriate response
     }
 };
+  
