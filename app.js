807dcdd185b4d60a269a7873522d1e12d89ad8b7
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-16 14:44:37 +0530

    Code started

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express'); // Importing the node express library
+
+const app = express(); // Intitalizing the app variable with the express library function
+
+
+
+
+
+module.exports = app;
