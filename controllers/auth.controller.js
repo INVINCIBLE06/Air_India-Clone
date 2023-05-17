9da9b51c30c68295124bdd452a89fd711704cd58
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-17 14:03:59 +0530

    Data is now being entered in the junction table

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,52 +4,48 @@ const jwt = require('jsonwebtoken');
 const db = require('../models');
 const config = require('../configs/auth.config');
 const fetch = require('../helper/commonFetching');
-
 const User = db.user;
 const Add = db.address
 
-
-exports.registration = async (req, res) => 
+exports.registration = async (req, res) =>  
 {
     try
     {
         let user_data = 
         {
-            name: req.body.name,
-            username: req.body.username,
-            email: req.body.email,
-            contact_no: req.body.contact_no,
-            password: req.body.password,
-            date_of_birth: req.body.date_of_birth,
+            name : req.body.name,
+            username : req.body.username,
+            email : req.body.email,
+            contact_no : req.body.contact_no,
+            password : bcrypt.hashSync(req.body.password, 8),
+            date_of_birth : req.body.date_of_birth,
         };
-        
-        const user = await User.create(user_data);
-        
+        const user = await User.create(user_data);        
         if(user)
         {
             console.log(`#### New User '${user.name}' created ####`);
-            let id = await fetch.userDetailsByEmail(req.body.email);
+            let uid = await fetch.userDetailsByEmail(req.body.email);
+            // console.log("Here :- ", id)
             let address_data = 
             {
+                user_Id : user.dataValues.id, 
                 house_no: req.body.house_no,
                 street_name: req.body.street_name,
                 city: req.body.city,
                 district: req.body.district,
                 state: req.body.state,
+                pin_code : req.body.pin_code, 
                 country: req.body.country,
             };
             const address = await Add.create(address_data);
             if(address)
             {
-                console.log(`#### '${user.name}' address has been added in the database ####`);
-                await user.addAddress
-                ({
-                    userId : user.id,
-                    addressId : address.id
-                }); // Add the association between user and address
-                console.log(`#### Association between '${user.name}' and the address has been created ####`);
+                console.log(`#### '${user.name}' address has been added in the database ####`);                
+                // let aid = await fetch.addressDetailsByid(user.dataValues.id); 
+                
+                await user.addAddress(address);
             }
-            else
+            else 
             {
                 console.log(`#### Failed to add the address of new user '${user.name}' ####`);
             }
@@ -61,8 +57,7 @@ exports.registration = async (req, res) =>
     }
     catch(error)
     {
-      console.log('Error during registration:', error);
-      // Handle the error and send an appropriate response
+      console.log('Error during registration:', error); // Handle the error and send an appropriate response
     }
 };
-  
+
