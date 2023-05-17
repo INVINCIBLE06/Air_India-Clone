5fa47ed673d2bcd54504bd2f29e34e491d21b3cf
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-17 14:03:20 +0530

    details on id, contact number help file is added

--- helper/commonFetching.js
+++ helper/commonFetching.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const User = db.user;
+const Add = db.address
 
 exports.userDetailsByEmail = async (email) => 
 {
@@ -22,7 +23,60 @@ exports.userDetailsByEmail = async (email) =>
     }
     catch (error)
     {
-        console.log('Error retrieving user details:', error);
+        console.log('Error retrieving user details on the basis fo email:', error);
+        throw error;
+    }
+};
+
+exports.userDetailsByContactNo = async (phone_number) => 
+{
+    try
+    {
+        const details = await User.findOne
+        ({
+            where:
+            {
+                contact_no : phone_number
+            }
+        });
+
+        if (!details) 
+        {
+            console.log(`#### No user found with this phone_number ####`);
+        }
+        // console.log("Fetching :- ", details)
+        return details;
+    }
+    catch (error)
+    {
+        console.log('Error retrieving user details on the basis of phone_number:', error);
+        throw error;
+    }
+};
+
+exports.addressDetailsByid = async (id) =>
+{
+    try 
+    {
+        const Addressdetails = await Add.findOne
+        ({
+            where:
+            {
+                user_Id : id
+            }
+        });
+
+        if(!Addressdetails)
+        {
+            console.log(`#### No address found with this user Id ####`);
+        }
+
+        // console.log("Fetching Address :- ", Addressdetails)
+        return Addressdetails;
+    }
+    catch(error)
+    {
+        console.log('Error retrieving address details:', error);
         throw error;
     }
 };
