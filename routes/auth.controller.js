85be6317e73833ca8f469dbf977f4b7dcfefde08
[Saurabh Pande] <[sp832154@gmail.com]>
2023-05-17 18:54:37 +0530

    Sigin route is written

--- routes/auth.controller.js
+++ routes/auth.controller.js
@@ -1,7 +1,9 @@
 const authController = require('../controllers/auth.controller');
+const {verifyBody} = require('../middlewares')
 
 
-module.exports = (app)=>
+module.exports = (app)=> 
 {
-    app.post(`/flight/booking/clone/registeration`, authController.registration);
+    app.post(`/flight/booking/clone/registeration`, [verifyBody.ValueEnteredCheck, verifyBody.ValidateSignUpRequestBody, verifyBody.DuplicateValueCheck], authController.registration);
+    app.post(`/flight/booking/clone/login`,authController.login);
 };
